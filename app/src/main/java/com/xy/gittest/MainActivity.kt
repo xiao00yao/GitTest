46fd2a2f8439163dc453fd41b89909c91b68987b
xiao00yao <1448363422@qq.com>
2020-09-03 11:31:18 +0800

    分支测试

---
+++ app/src/main/java/com/xy/gittest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xy.gittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /**
+         * 我是主分支
+         */
+    }
+}
